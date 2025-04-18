b8117952e190b7f2fcab1748ea909e63b5e3482c
Vitalii Smal <vitalii.smal@gmail.com>
2025-04-18 10:41:58 +0300

    added GitHub Action for Unit Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.adarshr.test-logger") version "4.0.0"
 }
 
 android {
